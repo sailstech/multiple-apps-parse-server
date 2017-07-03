ed09a73a3cb33083205a42a3a5ce3ad7d6ab7c8a
Richard Chin <richjing@gmail.com>
2017-07-03 11:04:48 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+// config
+var config = require('./config');
+
+// parse server
+var ParseServer = require('parse-server').ParseServer;
+// parse server dashbaord
+var ParseDashboard = require('parse-dashboard');
+
+var api = new ParseServer({
+    appName:config.param.parseServer.appName,
+    databaseURI: config.param.parseServer.databaseURI, // Connection string for your MongoDB database
+    cloud: config.param.parseServer.cloud, // Absolute path to your Cloud Code
+    appId: config.param.parseServer.appId,
+    masterKey: config.param.parseServer.masterKey, // Keep this key secret!
+    serverURL: config.param.parseServer.serverURL // Don't forget to change to https if needed
+});
+
+//parse server
+app.use('/parse', api);
+
+app.listen(config.param.parseServer.port, function() {
+    console.log('parse-server-example running on port '+config.param.parseServer.port);
+});
+//parse server dashboard
+var dashboard = new ParseDashboard({
+    "apps": [
+        {
+            "serverURL": config.param.parseServer.serverURL,
+            "appId": config.param.parseServer.appId,
+            "masterKey": config.param.parseServer.masterKey,
+            "appName": config.param.parseServer.appName
+        }
+    ]
+});
+app.use('/', dashboard);
+
+var httpServer = require('http').createServer(app);
+httpServer.listen(config.param.parseServer.dashboard_port);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
